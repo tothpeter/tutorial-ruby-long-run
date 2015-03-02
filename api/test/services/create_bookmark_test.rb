a00f81e20cd3213160540cfc63a9fa3237b9858b
Peter Toth <tothpeter08@gmail.com>
2015-03-02 21:13:05 +0100

    Add validation for bookmarks

--- api/test/services/create_bookmark_test.rb
+++ api/test/services/create_bookmark_test.rb
@@ -6,4 +6,8 @@ describe CreateBookmark do
   it "creates a bookmark" do
     assert CreateBookmark.new({ title: "Tomaaa.com", url: "http://tomaaa.com"}).create
   end
+
+  it "fails to create a bookmark if there's no data" do
+    refute CreateBookmark.new({}).create
+  end
 end
